ed1750cf532ad38b5c260fcb5a98e3d9d8f58e46
Sunny <sunny@Sunnys-MacBook-Pro.local>
2025-02-25 21:06:28 +1100

    added products, categories and their models and factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'price' => $this->faker->randomFloat(2, 10, 1000), // Random price between 10 and 1000
+            'category_id' => Category::factory(), // Creates a category if not provided
+        ];
+    }
+}
